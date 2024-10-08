0b41f266f938719ec1b989f675795ba09985c2f7
Adib Ahmed <63503701+AdibAhmed317@users.noreply.github.com>
2024-10-08 11:12:18 +0600

    subtitle removed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -20,7 +20,7 @@ const ExpenseTracker = () => {
 
   return (
     <Card className={classes.root}>
-      <CardHeader title='Expense Tracker' subheader='Powered by Speechly' />
+      <CardHeader title='Expense Tracker'/>
       <CardContent>
         <Typography align='center' variant='h5'>
           Total Balance ${balance}
